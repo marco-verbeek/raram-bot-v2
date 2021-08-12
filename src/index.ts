3a389d349c4a991dd12359bfa9329fe1f69aba2c
Marco <marcoverbeek99@gmail.com>
2021-08-12 14:37:19 +0200

    Added commands to index

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+const Commando = require('discord.js-commando');
+const path = require('path');
+
+require('dotenv').config()
+
+const client = new Commando.Client({
+  owner: process.env.DISCORD_OWNER_ID,
+  commandPrefix: '!',
+  disableEveryone: true
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}.`);
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['raram', 'Commands related to rARAM'],
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    help: false,
+    prefix: false,
+    ping: true,
+    eval: true,
+    unknownCommand: false,
+    commandState: false
+  })
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
+
+client.login(process.env.DISCORD_TOKEN);
