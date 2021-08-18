99e87c31049cac3d29dcad02b3c594384eac9166
Marco <marcoverbeek99@gmail.com>
2021-08-18 15:15:18 +0200

    Fixed embed creator typo

--- utils/embed_creator.ts
+++ utils/embed_creator.ts
@@ -1,16 +1,16 @@
 const { MessageEmbed } = require("discord.js");
 
 export enum embedType {
-  Info = "0x009FFF",
-  Success = "0x00FF00",
-  Error = "0xFF0000",
+  Info = 0x009FFF,
+  Success = 0x00FF00,
+  Error = 0xFF0000,
 }
 
 export function createEmbed(title: string, description: string, type = embedType.Info) {
   const embed = new MessageEmbed()
     .setAuthor(title)
     .setDescription(description)
-    .setColor(embedType)
+    .setColor(type)
 
   return embed;
 }
