181294b55edd79f592997ceb6207335e45050609
Marco <marcoverbeek99@gmail.com>
2021-09-17 13:01:42 +0200

    Added display of new champion stats

--- utils/stats_creator.ts
+++ utils/stats_creator.ts
@@ -2,15 +2,22 @@ export function displayChampStats(championStats: JSON): string[] {
   let col1 = "", col2 = "", col3 = "";
 
   col1 = `W/R: ${championStats["gamesWon"]}W, ${championStats["gamesPlayed"] - championStats["gamesWon"]}L
-          Penta kills: ${championStats["pentas"]}
-          Avg KP: ${championStats["totalKP"] / championStats["gamesPlayed"]}`;
+          Avg KP: ${championStats["totalKP"] / championStats["gamesPlayed"]}
+          Avg LP per Win: ${(championStats["totalPointsWon"] / championStats["gamesWon"]).toFixed(0)}
+          Avg LP per Lose: ${(championStats["totalPointsLost"] / (championStats["gamesPlayed"] - championStats["gamesWon"])).toFixed(0)}
+          Penta kills: ${championStats["pentaKills"]}`;
 
-  col2 = `Avg Damage Done: ${(championStats["totalDamageDone"] / championStats["gamesPlayed"]).toFixed(0)}
+  col2 = `Triple kills: ${championStats["tripleKills"]}
+          Quadra kills: ${championStats["quadraKills"]}
+          Avg Damage Done: ${(championStats["totalDamageDone"] / championStats["gamesPlayed"]).toFixed(0)}
           Avg Damage Taken: ${(championStats["totalDamageTaken"] / championStats["gamesPlayed"]).toFixed(0)}
           Avg Healing Done: ${(championStats["totalHealed"] / championStats["gamesPlayed"]).toFixed(0)}`;
 
-  Object.keys(championStats["players"]).forEach((key) => {
-    col3 += key + ", " + championStats["players"][key] + "x\n";
+  Object.keys(championStats["players"]).forEach((key) => {    
+    const gamesPlayed = championStats["players"][key][1][0];
+    const gamesWon = championStats["players"][key][1][1];    
+    
+    col3 += `${championStats["players"][key][0]}, ${gamesWon}W ${gamesPlayed - gamesWon}L\n`;
   })
 
   return [col1, col2, col3];
